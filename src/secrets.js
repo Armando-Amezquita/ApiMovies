b9a7ad694b8e5d676b1d12ae7fc442862324ffb6
Armando Amezquita <amolina@smdigital.com.co>
2022-07-12 18:06:43 -0500

    Configuracion inicial del proyecto

---
+++ src/secrets.js
@@ -0,0 +1,5 @@
+//Para el correcto funcionamiento de la aplicación:
+// 1. Crear una cuenta en https://www.themoviedb.org/
+// 2. Obtener el API-KEY que ellos otorgan.
+// 3. Reemplazar donde se utiliza la API-KEY por la que les dió la aplicación.
+// 4. En el archivo de inde.html se debe reemplazar el script de secretsKey.js, por el nombre de este archivo, o simplemente no incorporar este archivo en todas las variables donde este el API-KEY reemplazarlo por la de uds. 
