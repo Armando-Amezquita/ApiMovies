e336c09c147f907e17e7283b34163b5bb415c2f4
Armando Amezquita <amolina@smdigital.com.co>
2022-07-15 17:58:23 -0500

    Creacion y estructura de la aplicacion, junto con la navegacion y la creacion de cada vista

--- src/main.js
+++ src/main.js
@@ -1,1 +1,67 @@
-console.log('hola')
+const API = "https://api.themoviedb.org/3";
+
+//NOTA:
+//  Cada vez que se ejecuta la navegacion es decir que se da click en algun boton y ello lleva a otra pagina y luego volvemos a la misma, se va a duplicar todo lo que se llame de la API, es decir que se debe limpiar ese nodo para que la informacion no se duplique
+
+//Fetch:
+    // const data = await fetch(`${API}/trending/movie/day?api_key=${API_KEY}`);
+    // const dataJson = await data.json();
+
+//Con AXIOS se pueden pasar los parametros que necesita la API para realizar la consulta, como el api_key.
+
+const axiosAPI = axios.create({
+  baseURL: API,
+  Headers: {
+    'Content-Type': 'application/json;charset=utl-8'
+  },
+  params: {
+    api_key: API_KEY
+  }
+})
+
+
+async function getTrendingMoviesPreview(){
+    const data = await axiosAPI(`/trending/movie/day`);
+    // const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList');
+    // Este elemento se esta obteniendo desde el archivo nodes.js
+
+    trendingMoviesPreviewList.innerHTML = '';
+
+    data.data.results.forEach(movie => {
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+    
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', `https://image.tmdb.org/t/p/w300${movie.poster_path}`);
+    
+        movieContainer.appendChild(movieImg);
+        trendingMoviesPreviewList.appendChild(movieContainer);
+        
+    });
+}
+
+async function getGenresMoviesPreview(){
+
+    const data = await axiosAPI(`/genre/movie/list`);
+    // const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+    //Con esta linea se limpia el codigo que ya tenia cuando se haga la navegacion
+    categoriesPreviewList.innerHTML = '';
+
+    data.data.genres.forEach(genre => {
+        const genreContainer = document.createElement('div');
+        genreContainer.classList.add('category-container');
+
+        const genreTitle = document.createElement('h3');
+        const genreText = document.createTextNode(genre.name);
+        genreTitle.appendChild(genreText);
+        genreTitle.classList.add('category-title');
+        genreTitle.setAttribute('id', `id${genre.id}`);    
+    
+        genreContainer.appendChild(genreTitle);
+        categoriesPreviewList.appendChild(genreContainer);
+    });
+}
+
---
+++ src/navigation.js
@@ -0,0 +1,125 @@
+arrowBtn.addEventListener('click', () => {
+    location.hash = '#home';
+});
+
+trendingBtn.addEventListener('click', () => {
+    location.hash = '#trends=';
+});
+
+searchFormBtn.addEventListener('click', () =>{
+    location.hash = '#search='
+})
+
+//DOMContentLoaded
+//Se encarga de ejecutar la funcion de navigator apenas carga la aplicacion.
+window.addEventListener('DOMContentLoaded', navigator, false);
+//hashchange
+//Se encarga de visualizar cual es el hash de la ruta y renderizar o ejecutar la función de esa ruta especifica.
+window.addEventListener('hashchange', navigator, false);
+
+//location:
+//Permite saber al navegador en que vista esta el usuario, tiene una propiedad llamada hash.
+//hash:
+//Hace refenrencia al #final de la URL ejemplo: https://platzi#cursos, cursos seria el hash
+
+//Esta es la logica de navegacion, para que de acuerdo al hash que se utilice, ejecute diferente funcion y renderice la informacion de esa funcion.
+function navigator(){
+    console.log({ location });
+    //Pregunta si el hash de la url empieza con trends para ejecutar la funcion trends
+    if(location.hash.startsWith('#trends')){
+        trendsPage();
+    } else if(location.hash.startsWith('#search=')){
+        searchPage();
+    } else if(location.hash.startsWith('#movie=')){
+        movieDetailsPage();
+    } else if(location.hash.startsWith('#category=')){
+        categoriesPage();
+    } else{
+        homePage();
+    }
+}
+
+function homePage(){
+    console.log('Home')
+
+    //Estas categorias las trae del archivo nodes.js
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.backgound = '';
+    arrowBtn.classList.add('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerTitle.classList.remove('inactive');
+    headerCategoryTitle.classList.add('inactive');
+    searchForm.classList.remove('inactive');
+
+    trendingPreviewSection.classList.remove('inactive');
+    categoriesPreviewSection.classList.remove('inactive');
+
+    genericSection.classList.add('inactive');
+    movieDetailSection.classList.add('inactive');
+
+    getTrendingMoviesPreview();
+    getGenresMoviesPreview();
+}
+
+function categoriesPage(){
+    
+    console.log('Category ')
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.backgound = '';
+    arrowBtn.classList.remove('inactive');
+    headerTitle.classList.add('inactive');
+    headerCategoryTitle.classList.remove('inactive');
+    searchForm.classList.remove('inactive');
+
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.remove('inactive');
+
+    genericSection.classList.remove('inactive');
+    movieDetailSection.classList.add('inactive');
+}
+
+function movieDetailsPage(){
+    
+    console.log('movie ')
+    headerSection.classList.add('header-container--long');
+    headerSection.style.backgound = '';
+    arrowBtn.classList.remove('inactive');
+    arrowBtn.classList.add('header-arrow--white');
+    headerTitle.classList.add('inactive');
+    headerCategoryTitle.classList.add('inactive');
+    searchForm.classList.add('inactive');
+    trendingPreviewSection.classList.remove('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.add('inactive');
+    movieDetailSection.classList.remove('inactive');
+}
+
+function searchPage(){
+    console.log('search');
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.backgound = '';
+    arrowBtn.classList.remove('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerTitle.classList.add('inactive');
+    headerCategoryTitle.classList.add('inactive');
+    searchForm.classList.remove('inactive');
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.add('inactive');
+    movieDetailSection.classList.add('inactive');
+}
+
+function trendsPage(){
+    console.log('Trends');
+    headerSection.classList.remove('header-container--long');
+    // headerSection.style.backgound = '';
+    arrowBtn.classList.remove('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerTitle.classList.add('inactive');
+    headerCategoryTitle.classList.remove('inactive');
+    searchForm.classList.add('inactive');
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.remove('inactive');
+    movieDetailSection.classList.add('inactive');
+}
